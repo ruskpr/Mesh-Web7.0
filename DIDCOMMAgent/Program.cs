46438075b4fc52997c2ee80ea725d7b86e9671f9
Rustu Koprulu <rustulp@gmail.com>
2023-02-27 20:42:08 -0700

    added

---
+++ DIDCOMMAgent/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DIDCOMMAgent
+{
+    internal class Program
+    {
+        #region DidComm endpoint handler
+
+        class DIDCOMMAgent : DIDCOMMAgentBase
+        {
+            public override void DIDCOMMEndpointHandler(DIDCOMMMessage request, out DIDCOMMResponse response)
+            {
+                response.rc = (int)Trinity.TrinityErrorCode.E_SUCCESS;
+            }
+        }
+
+        #endregion
+
+        #region Main()
+
+        public static void Main(string[] args)
+        {
+            Trinity.TrinityConfig.HttpPort = 8081;
+
+            DIDCOMMAgent didAgent = new DIDCOMMAgent();
+            didAgent.Start();
+            Console.WriteLine("DIDCOMM Agent start...");
+
+            Console.WriteLine("Press Enter to stop DIDCOMM Agent...");
+            Console.ReadLine();
+
+            didAgent.Stop();
+        }
+
+        #endregion
+
+    }
+}
