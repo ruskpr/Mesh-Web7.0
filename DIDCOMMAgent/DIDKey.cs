389d699c5749d4a633b15a360d10d572c229edf5
Russ Koprulu <rustulp@gmail.com>
2023-03-15 14:15:19 -0600

    added

---
+++ DIDCOMMAgent/DIDKey.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using Okapi.Keys;
+using Okapi.Keys.V1;
+using System.Text.Json;
+using Okapi.Security;
+using Okapi.Security.V1;
+
+namespace DIDCOMMAgent
+{
+    public class DIDKey
+    {
+
+        #region props
+
+        public CreateOberonKeyResponse ProofKey { get; set; }
+        public string KeyFilePath { get => _keyFilePath; }
+        public bool IsInitialized { get => _isInitialized; }
+
+        #endregion
+
+        #region fields
+
+        private Subject _subject;
+        private string _keyFilePath;
+        private bool _isInitialized = false;
+        public JsonWebKey SecretKey;
+        public JsonWebKey PublicKey = new JsonWebKey();
+        public string KeyId;
+
+        #endregion
+
+        #region constructor
+
+        public DIDKey(Subject subject)
+        {
+            _subject = subject;
+
+            // temporary key store
+            _keyFilePath = $"e:\\didcomm\\keys\\{_subject.Name.ToLower()}.keyfile.json";
+        }
+
+        #endregion
+
+        #region public
+
+        public void InitKey()
+        {
+
+            if (_isInitialized) return;
+            _isInitialized = true;
+
+            JsonWebKey didWebKey;
+
+            if (!File.Exists(_keyFilePath))
+            {
+                GenerateKeyResponse didKey = Okapi.Keys.DIDKey.Generate(new GenerateKeyRequest { KeyType = KeyType.X25519 });
+                didWebKey = didKey.Key[0];
+                string didKeyJson = JsonSerializer.Serialize(didWebKey);
+                File.WriteAllText(_keyFilePath, didKeyJson);
+            }
+            else
+            {
+                string didKeyJson = File.ReadAllText(_keyFilePath);
+                didWebKey = JsonSerializer.Deserialize<JsonWebKey>(didKeyJson);
+            }
+
+            // set secret key
+            SecretKey = new JsonWebKey()
+            {
+                Kid = didWebKey.Kid,
+                Kty = didWebKey.Kty,
+                Crv = didWebKey.Crv,
+                D = didWebKey.D,
+            };
+
+            // set public key
+            PublicKey = new JsonWebKey()
+            {
+                Kid = didWebKey.Kid,
+                Kty = didWebKey.Kty,
+                Crv = didWebKey.Crv,
+                X = didWebKey.X,
+            };
+            
+
+            KeyId = didWebKey.Kid;
+            ProofKey = Oberon.CreateKey(new CreateOberonKeyRequest());
+        }
+
+        #endregion
+
+    }
+
+    
+}
---
+++ DIDCOMMAgent/Subject.cs
@@ -0,0 +1,55 @@
+﻿using Google.Protobuf;
+using Okapi.Keys.V1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIDCOMMAgent
+{
+    public class Subject
+    {
+        #region static
+        // key is subjects Key ID
+        public static Dictionary<string, Subject> Subjects = new Dictionary<string, Subject>();
+
+        #endregion
+
+        #region props
+
+        public string Name { get; set; }
+        public JsonWebKey MsgPublicKey { get; set; }    // public key: Identity, Message Encryption/Decryption
+        public JsonWebKey MsgSecretKey { get; set; }    // secret key: Identity, Message Encryption/Decryption
+        public ByteString ProofPublicKey { get; set; }  // Creation/Verification of Credential Proofs
+        public ByteString ProofSecretKey { get; set; }  // Creation/Verification of Credential Proofs
+
+        public DIDKey DIDKey { get; }
+
+        #endregion
+
+        #region constructor
+
+        public Subject(string name)
+        {
+            Name = name;
+
+            // init key by passing in subject
+            DIDKey key = new DIDKey(this);
+            key.InitKey();
+
+            //set subject's public and secret keys
+            DIDKey = key;
+            MsgPublicKey = DIDKey.PublicKey;
+            MsgSecretKey = DIDKey.SecretKey;
+            ProofPublicKey = DIDKey.ProofKey.Pk;
+            ProofSecretKey = DIDKey.ProofKey.Sk;
+
+            // add subject to static 'Subjects' dictionary
+            Subjects.Add(key.KeyId, this);
+        }
+
+        #endregion
+
+    }
+}
