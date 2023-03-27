95c53d2f3a741ee4fab0ba4ee77458bf3b56b7ee
Russ Koprulu <rustulp@gmail.com>
2023-03-27 14:47:10 -0600

    added port configuration

---
+++ Mesh_App/AppSettings.cs
@@ -0,0 +1,26 @@
+﻿using System.Configuration;
+
+namespace Mesh_App
+{
+    public static class AppSettings
+    {
+
+        // port that the didcomm agent will use to listen for incoming messages
+        public static int AgentPort
+        {
+            get
+            {
+                return int.Parse(ConfigurationManager.AppSettings["Port"]);
+            }
+            set
+            {
+                Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                config.AppSettings.Settings["Port"].Value = value.ToString();
+                config.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection("appSettings");
+            }
+        }
+
+        
+    }
+}
